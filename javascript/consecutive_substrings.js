16cd588f00d29621104a832847348f71b1379f87
Scott Schwab <scottschwab86@gmail.com>
2024-02-07 10:00:41 -0600

    Complete challenge

--- javascript/consecutive_substrings.js
+++ javascript/consecutive_substrings.js
@@ -1,5 +1,22 @@
 function consecutiveSubstrings(string) {
-  // type your code here
+  // have results arr
+  // iterate over the string
+  // for each character
+    // iterate from that character to the end of the string
+    // create substring of that character to each subsequent character
+    // push that substring to results arr
+  // return results arr
+
+  let resultsArr = [];
+
+  for (let i = 0; i < string.length; i++) {
+    for (let j = i; j < string.length; j++) {
+      let sub = string.substring(i, j+1)
+      resultsArr.push(sub)
+    }
+  }
+
+  return resultsArr;
 }
 
 if (require.main === module) {
@@ -11,6 +28,16 @@ if (require.main === module) {
 
   console.log("Expecting: ['a']");
   console.log("=>", consecutiveSubstrings('a'));
+
+  console.log("");
+
+  console.log("Expecting: []");
+  console.log("=>", consecutiveSubstrings(''));
+
+  console.log("");
+
+  console.log("Expecting: ['a', 'aa', 'aab', 'a', 'ab', 'b']");
+  console.log("=>", consecutiveSubstrings('aab'));
 }
 
 module.exports = consecutiveSubstrings;
